173e6b2eccfdf41897ad1ecae210edfbfb230b23
Ricard0-A <ricardoarias4@hotmail.com>
2024-10-22 02:36:50 +0000

    fixed

--- src/js/app.js
+++ src/js/app.js
@@ -39,7 +39,7 @@ function render(variables = {}) {
           <h2>${variables.role == null ? "Web Developer" : variables.role}</h2>
           <h3>${variables.city == null ? "Munich" : variables.city}
            ${variables.country == null ? "Germany" : variables.country}</h3>
-          <ul class="position-right">
+          <ul class="${variables.socialMediaPosition}">
             <li><a href="https://twitter.com/${
               variables.twitter
             }"><i class="fab fa-twitter"></i></a></li>
